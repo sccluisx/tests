cee0b899e47a18493d391ac8d2d58994762b7238
Luis Enrique Ramirez Chavez <lramirez@mpi-inf.mpg.de>
2016-02-19 04:41:15 +0100

    fix of run_jellyfish

--- seecer/repo_ver/SEECER/bin/run_jellyfish.sh
+++ seecer/repo_ver/SEECER/bin/run_jellyfish.sh
@@ -5,18 +5,18 @@ TMPDIR=$5
 
 if [ "$#" -eq "4" ];
 then
-$JF count -m $3 -o $TMPDIR/jf_tmp -c 3 -s 10000000 -t 32 --both-strands $6
+$JF count -m $3 -o $TMPDIR/jf_tmp -c 3 -s 10000000 -t 32 --canonical $6
 else
-$JF count -m $3 -o $TMPDIR/jf_tmp -c 3 -s 10000000 -t 32 --both-strands $6 $7
+$JF count -m $3 -o $TMPDIR/jf_tmp -c 3 -s 10000000 -t 32 --canonical $6 $7
 fi;
 
 # merge
-N_TMP=`ls -1 $TMPDIR/jf_tmp_* | wc -l`
+N_TMP=`ls -1 $TMPDIR/jf_tmp* | wc -l`
 if [ $N_TMP -eq 1 ]
 then
-    mv $TMPDIR/jf_tmp_0 $TMPDIR/jf_merged_$3
+    mv $TMPDIR/jf_tmp $TMPDIR/jf_merged_$3
 else
-    $JF merge $TMPDIR/jf_tmp_* -o $TMPDIR/jf_merged_$3
+    $JF merge $TMPDIR/jf_tmp* -o $TMPDIR/jf_merged_$3
     rm $TMPDIR/jf_tmp_*
 fi
 
